73e892d7c5fedce72967caa3b2d74b0f649934c5
Glitch (dream-journal) <none>
2018-09-04 16:20:57 +0000

    Migrated to MongoDB

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 Dream Journal
 ==============
 
-Using this simple app you can keep  track of your dreams.
-Just click `Add Dream` button and enter the description. There will be more functionality in the future.
+Using this simple app you can keep track of your dreams.
 
+Click `Add Dream` button and enter a short title for your dream as well as some additional data:
++ _Date and Time_ section can be used to enter your sleeping schedule. 
++ If the Dream was Lucid, choose lucidity technique - _MILD_ or _WILD_ -  you have used to keep your conciousness during the Dream, and choose _None_ if it wasn't Lucid. More on Lucidity in upcoming `Info` section of the app.
++ _Setting_ field is for you to enter location of the Dream. 
 
-\ ゜o゜)ノ
+All these additional fields (more in the future) will be consolidated into statistical data in upcoming `User` section, so they can help you shape your practices towards learning Lucid Dreaming.
+
+\ (゜o゜)ノ
+
+The app is being created with Node, Express, Mongoose and Pug.
--- routes/users.js
+++ routes/users.js
@@ -3,29 +3,38 @@ const router = express.Router();
 const session = require('express-session');
 const flash = require('connect-flash');
 const { check, validationResult } = require('express-validator/check');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./.data/sqlite.db');
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.db, { useNewUrlParser: true });
+const User = require('../models/user');
 
 router.get('/register', function(request, response) {
+  if (request.user) {
+    request.logout();
+    request.flash('succ', 'You have been logged out');
+  }
   response.render('register');
 });
 
 const isUnique = (value, {req, location, path}) => {
   return new Promise((resolve, reject) => {
-    if (!value) resolve(true); //do not proceed with validation if username is empty
-    const select = db.prepare(`SELECT ${path} FROM Users WHERE ${path}=(?)`);
-    select.get(value, (err, row) => {
-      if (err) reject(err);
-      resolve(!row);
+    if (value.length < 5) resolve(true); //do not proceed with validation if username is less than 5 char
+    User.findOne({[path]: value}, (err, user) => {
+      if (err || !user) resolve(true);
+      else reject(false);
     });
-  })
+  });
 };
 
 router.post('/register', [
-  check('username', 'Username should not be empty').not().isEmpty(),
+  check('username', 'Username should be at least 5 char').isLength({min: 5}),
   check('username', 'Username is already taken').custom(isUnique),
-  check('password', 'Password should not be empty').not().isEmpty(),
+  check('password', 'Password should be at least 6 char').isLength({min: 6}),
+  check('password', 'Password should contain both numbers and letters of both cases').custom(value => {
+    const regex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])');
+    return regex.test(value);
+  }),
   check('password2', 'Confirm password should not be empty').not().isEmpty(),
   check('password2', 'Passwords should match').custom((value, {req}) => value === req.body.password),
   check('email', 'Email is not valid').isEmail(),
@@ -38,13 +47,19 @@ router.post('/register', [
     });
     response.render('register', {} = request.body);
   } else {
-    const insert = db.prepare('INSERT INTO Users (username, password, email) VALUES (?, ?, ?)');
-    bcrypt.hash(request.body.password, 10, function(err, hash) {
+    let user = new User();
+    user.username = request.body.username;
+    user.email = request.body.email;
+    bcrypt.hash(request.body.password, 10, (err, hash) => {
       if (err) console.error(err);
-      console.log('Adding user ', request.body.username);
-      insert.run([request.body.username, hash, request.body.email]);
-      request.flash('succ', `User '${request.body.username}' created`);
-      response.render('login', {username: request.body.username});
+      user.password = hash;
+      user.save(err => {
+        if (err) console.error(err);
+        else {
+          request.flash('succ', `User ${request.body.username} created`);
+          response.render('login', {username: request.body.username});
+        }
+      });
     });
   }
 });
@@ -53,50 +68,19 @@ router.get('/login', function(request, response) {
   response.render('login');
 });
 
-const validateUN = (value) => {
-  return new Promise((resolve, reject) => {
-    if (!value) resolve(true); //not proceed with validation if username is empty
-    const select = db.prepare('SELECT username FROM Users WHERE username=(?)');
-    select.get(value, (err, row) => {
-      if (err) reject(err);
-      if (row) resolve(true);
-      else reject(false);
-    });
-  });
-}
-const validatePW = (value, {req}) => {
-  return new Promise((resolve, reject) => {
-    if (!req.body.username) resolve(true); //not proceed with validation if username is empty
-    const select = db.prepare('SELECT password FROM Users WHERE username=(?)');
-    select.get(req.body.username, (err, row) => {
-      if (err) reject(err);
-      if (!row) resolve(true); //not proceed with validation if no result from query
-      else resolve(row);
-    });
-  }).then((result) => {
-    if (result === true) return true; //catching resolve(true) from previous promise body and passing check
-    return bcrypt.compare(value, result.password).then((res) => {
-      return res;
-    });
-  });
-};
+router.post('/login', function(request, response, next) {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+    badRequestMessage: 'Missing username or password',
+    failureFlash: true
+  })(request, response, next);
+});
 
-router.post('/login', [
-  check('username', 'Username should not be empty').not().isEmpty(),
-  check('username', 'Username not found').custom(validateUN),
-  check('password', 'Password should not be empty').not().isEmpty(),
-  check('password', 'Password is incorrect').custom(validatePW)
-], function(request, response) {
-  const errors = validationResult(request);
-  if(!errors.isEmpty()) {
-    errors.array().forEach((err) => {
-      request.flash('errr', err.msg);
-    });
-    response.render('login', {username: request.body.username});
-  } else {
-    request.flash('succ', 'Login successful');
-    response.redirect('/');
-  }
+router.get('/logout', function(request, response) {
+  request.logout();
+  request.flash('succ', 'You are logged out');
+  response.redirect('/users/login');
 });
 
 module.exports = router;
